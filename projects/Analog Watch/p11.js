fe6f2212f25238ec1e038f8941298bbdd283a561
Girish Chandra Meher <raju.meher.7258@gmail.com>
2026-07-14 10:46:33 +0530

    Enhance clock function with digital time and date
    Added digital clock display and date formatting.

--- projects/Analog Watch/p11.js
+++ projects/Analog Watch/p11.js
@@ -1,19 +1,37 @@
 let minHand = document.querySelector('.min-hand');
 let hourHand = document.querySelector('.hour-hand');
 let secHand = document.querySelector('.second-hand');
-function clock(){
+
+let digitalTime = document.getElementById("digital-time");
+let date = document.getElementById("date");
+
+function clock() {
     let time = new Date();
+
     let hrs = time.getHours();
     let mins = time.getMinutes();
-    hrs = (((hrs*60)/12)*6)+90;   
+
+    hrs = (((hrs * 60) / 12) * 6) + 90;
     hourHand.style.transform = `rotate(${hrs}deg)`;
-    
-    mins=(mins*6)+90;
+
+    mins = (mins * 6) + 90;
     minHand.style.transform = `rotate(${mins}deg)`;
 
     let sec = time.getSeconds();
-    sec = (sec*6)+90;
+    sec = (sec * 6) + 90;
     secHand.style.transform = `rotate(${sec}deg)`;
+
+    // Digital Time
+    digitalTime.textContent = time.toLocaleTimeString();
+
+    // Date
+    date.textContent = time.toLocaleDateString("en-US", {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    });
 }
-setInterval(clock,1000)
+
+setInterval(clock, 1000);
 clock();
