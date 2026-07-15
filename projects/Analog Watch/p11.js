8f15141e5258f374b700e365b755e212456fc611
Girish Chandra Meher <raju.meher.7258@gmail.com>
2026-07-15 14:23:07 +0530

    Enhance clock with digital time and calendar features
    Added digital clock display and calendar input functionality.

--- projects/Analog Watch/p11.js
+++ projects/Analog Watch/p11.js
@@ -1,19 +1,39 @@
 let minHand = document.querySelector('.min-hand');
 let hourHand = document.querySelector('.hour-hand');
 let secHand = document.querySelector('.second-hand');
+let digitalTime = document.getElementById('digital-time');
+let digitalDate = document.getElementById('digital-date');
+let calendarInput = document.getElementById('calendar');
+
 function clock(){
     let time = new Date();
+    
+    // Analog Clock
     let hrs = time.getHours();
     let mins = time.getMinutes();
-    hrs = (((hrs*60)/12)*6)+90;   
-    hourHand.style.transform = `rotate(${hrs}deg)`;
+    let hrsDeg = (((hrs*60)/12)*6)+90;   
+    hourHand.style.transform = `rotate(${hrsDeg}deg)`;
     
-    mins=(mins*6)+90;
-    minHand.style.transform = `rotate(${mins}deg)`;
+    let minsDeg=(mins*6)+90;
+    minHand.style.transform = `rotate(${minsDeg}deg)`;
 
     let sec = time.getSeconds();
-    sec = (sec*6)+90;
-    secHand.style.transform = `rotate(${sec}deg)`;
+    let secDeg = (sec*6)+90;
+    secHand.style.transform = `rotate(${secDeg}deg)`;
+    
+    // Digital Time and Date
+    if (digitalTime) {
+        digitalTime.textContent = time.toLocaleTimeString();
+    }
+    if (digitalDate) {
+        digitalDate.textContent = time.toLocaleDateString(undefined, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+    }
+    
+    // Calendar initial value
+    if (calendarInput && !calendarInput.value) {
+        let isoDate = new Date(time.getTime() - (time.getTimezoneOffset() * 60000)).toISOString().split('T')[0];
+        calendarInput.value = isoDate;
+    }
 }
-setInterval(clock,1000)
+setInterval(clock,1000);
 clock();
